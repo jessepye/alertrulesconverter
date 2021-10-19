0dea92b434d8fb6a0e422f1708d0b6bc83992054
Jerry Belmonte <jbelmonte@vmware.com>
2021-10-18 18:04:17 -0700

    Add command line flags for alert config files

--- main.go
+++ main.go
@@ -2,11 +2,42 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 
 	"gopkg.in/yaml.v2"
 )
+
+var (
+	hw1File string
+	hw2File string
+)
+
+func init() {
+	flag.StringVar(&hw1File, "hw1-file", "hw1_alerting_rules.json", "The HW1 Alerting Rules File")
+	flag.StringVar(&hw2File, "hw2-file", "hw2_alerting_rules.yml", "The HW2 Alerting Rules File")
+	flag.Parse()
+}
+
+func main() {
+	hw1, err := LoadHW1Alerts(hw1File)
+	if err != nil {
+		fmt.Println(err)
+	}
+	hw1.Display()
+
+	hw2, err := LoadHW2AlertRules(hw2File)
+	if err != nil {
+		fmt.Println(err)
+	}
+	hw2.Display()
+}
+
+type HW1 struct {
+	ConfigFile string
+	HW1Alerts  []*HW1Alert
+}
 
 type HW1Alert struct {
 	Query     string    `json:"query"`
@@ -20,65 +51,74 @@ type Threshold struct {
 	Warning  float32 `json:"warning,omitempty"`
 }
 
-type HW2AlertRules struct {
-	AlertingRules []HW2AlertRule `yaml:"groups"`
+func (hw *HW1) Display() {
+	fmt.Printf("HW1 File: %s\n", hw.ConfigFile)
+	for _, alert := range hw.HW1Alerts {
+		fmt.Printf("Query: %s\n", alert.Query)
+	}
+}
+
+func LoadHW1Alerts(filename string) (*HW1, error) {
+	hw1 := &HW1{}
+
+	jsonFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	hw1.ConfigFile = filename
+
+	err = json.Unmarshal(jsonFile, &hw1.HW1Alerts)
+	if err != nil {
+		return nil, err
+	}
+	return hw1, nil
+}
+
+type HW2 struct {
+	ConfigFile    string          `yaml:"-"`
+	AlertingRules []*HW2AlertRule `yaml:"groups"`
 }
 
 type HW2AlertRule struct {
-	Name  string  `yaml:"name"`
-	Rules []Alert `yaml:"rules,flow"`
+	Name  string   `yaml:"name"`
+	Rules []*Alert `yaml:"rules,flow"`
 }
 
 type Alert struct {
-	Alert       string `yaml:"alert"`
-	Expression  string `yaml:"expr"`
-	For         string `yaml:"for"`
-	Annotations struct {
-		Summary     string
-		Description string
+	Alert       string      `yaml:"alert"`
+	Expression  string      `yaml:"expr"`
+	For         string      `yaml:"for"`
+	Annotations Annotations `yaml:",flow"`
+}
+
+type Annotations struct {
+	Summary     string `yaml:"summary"`
+	Description string `yaml:"description"`
+}
+
+func (hw *HW2) Display() {
+	fmt.Printf("HW2 File: %s\n", hw.ConfigFile)
+	for _, alertRule := range hw.AlertingRules {
+		fmt.Printf("- Name: %s\n", alertRule.Name)
+		for _, rule := range alertRule.Rules {
+			fmt.Printf("  - Alert: %s\n", rule.Alert)
+			fmt.Printf("    Expr: %s\n", rule.Expression)
+		}
 	}
 }
 
-func main() {
-	jsonFile, err := ioutil.ReadFile("hw1_alerting_rules.json")
+func LoadHW2AlertRules(filename string) (*HW2, error) {
+	hw2 := &HW2{}
+
+	yamlFile, err := ioutil.ReadFile(filename)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
+	hw2.ConfigFile = filename
 
-	var hw1Alerts []HW1Alert
-	err = json.Unmarshal(jsonFile, &hw1Alerts)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for _, v := range hw1Alerts {
-		s := v.Query
-		fmt.Printf("Query: %s\n", s)
-	}
-
-	yamlFile, err := ioutil.ReadFile("hw2_alerting_rules.yml")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	var hw2 HW2AlertRules
 	err = yaml.Unmarshal(yamlFile, &hw2)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-
-	var hw2Map = make(map[string]map[string]string)
-	for _, alertRule := range hw2.AlertingRules {
-		hw2Map[alertRule.Name] = map[string]string{}
-		for _, rule := range alertRule.Rules {
-			hw2Map[alertRule.Name][rule.Alert] = rule.Expression
-		}
-	}
-	for k, rules := range hw2Map {
-		fmt.Printf("- Name: %s\n", k)
-		for alert, expr := range rules {
-			fmt.Printf("  Alert: %s\n", alert)
-			fmt.Printf("  Expr: %s\n", expr)
-		}
-	}
+	return hw2, nil
 }
